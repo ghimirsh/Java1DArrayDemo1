d196027444980f21d91f83339854990679de8409
Shankar Ghimire <ghimirsh@sheridancollege.ca>
2020-01-14 13:38:13 -0500

    .gitignorefile added

---
+++ src/java1darraydemo1/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Shankar Ghimire
+ * StudentId:991585190
+ * Course: ...............
+ */
+package java1darraydemo1;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num[] = new int[5];
+        //input to the array
+        System.out.println("Enter any 5 numbers : ");
+        for(int i = 0; i < num.length; i++){
+            num[i] = scanner.nextInt();
+        }
+    }
+    
+}
